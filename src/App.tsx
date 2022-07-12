3559433fad716c7ec82efdfb0578051bc29bdf0c
Alexander Coronel <alexc957>
2022-07-12 13:52:16 -0500

    prject structure done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import First from "./components/First";
+import Second from "./components/Second";
+export default function App() {
+  return (
+    <div>
+      <p>hello world</p>
+      <First />
+      <Second />
+    </div>
+  );
+}
