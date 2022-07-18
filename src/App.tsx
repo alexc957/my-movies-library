fb08245ef54691d973dd0625580a3fce462da78d
Alexander Coronel <alexc957>
2022-07-18 08:06:40 -0500

    chakra ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import First from "./components/First";
 import Second from "./components/Second";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 export default function App() {
   return (
-    <div>
-      <p>hello world</p>
-      <First />
-      <Second />
-    </div>
+    <ChakraProvider>
+      <div>
+        <Button colorScheme="blue">Button</Button>
+        <p>hello world</p>
+        <First />
+        <Second />
+      </div>
+    </ChakraProvider>
   );
 }
