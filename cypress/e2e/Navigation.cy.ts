077b672818e9887f54779778432a73cf1644425b
Alexander Coronel <alexc957>
2022-07-25 07:18:16 -0500

    new test added and test formated

--- cypress/e2e/Navigation.cy.ts
+++ cypress/e2e/Navigation.cy.ts
@@ -1,15 +1,28 @@
 /// <reference types="cypress" />
 
-describe("when  navigating between the home and about", () => {
+describe("nav bar", () => {
   before(() => {
     cy.visit("http://localhost:8080");
   });
+  context("when  navigating between the home and about", () => {
+    it("must be a about link", () => {
+      cy.get('[data-cy="about"]').click();
+      cy.get('[data-cy="about-section"]').should(
+        "contain",
+        "This is the about a small web app"
+      );
+    });
+  });
+});
 
-  it("must be a about link", () => {
-    cy.get('[data-cy="about"]').click();
-    cy.get('[data-cy="about-section"]').should(
-      "contain",
-      "This is the about a small web app"
-    );
+describe("clicking on movie", () => {
+  before(() => {
+    cy.visit("http://localhost:8080");
+  });
+  context("when clicking on a movie title in the home", () => {
+    it("it should navigate to a new page with the movie details", () => {
+      cy.get('[data-ct="title"]').click();
+      cy.url().should("include", "/movie/:id");
+    });
   });
 });
