184de22dd20af12d6eecc84ac9c57ddd5680a8c1
alex <alex@pop-os.localdomain>
2022-08-15 21:25:19 -0500

    add validations and devtool to react hook form

--- src/pages/NewMovie/NewMovie.tsx
+++ src/pages/NewMovie/NewMovie.tsx
@@ -1,17 +1,18 @@
-import { Checkbox, Container, Input, Textarea } from "@chakra-ui/react";
+import { Button, Checkbox, Container, FormControl, FormErrorMessage, FormLabel, Input, Textarea } from "@chakra-ui/react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import Layout from "../../components/Layout/Layout";
 import { IMovieResult, INewMovie } from "../../interfaces/movies";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { DevTool } from '@hookform/devtools';
 
-const fields: string[] = [];
+
 
 const schema = yup
   .object({
     poster_path: yup.string().notRequired(),
-    adult: yup.boolean(),
+    adult: yup.boolean().default(false),
     overview: yup
       .string()
       .min(15, "min lenght is 15")
@@ -30,9 +31,11 @@ export default function NewMovie() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    control,
+    formState: { errors, isSubmitting },
   } = useForm<INewMovie>({
     resolver: yupResolver(schema),
+    mode: 'onChange', // -> validation strategy
   });
 
   const onSubmit = (data: INewMovie) => {
@@ -41,14 +44,75 @@ export default function NewMovie() {
 
   return (
     <Layout searchBar>
-      <Container>
+      <Container mb={"96"}>
         <h3>new movie</h3>
+        <DevTool control={control} placement="top-left" />
+        
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Input {...register("title")} />
-          <Textarea {...register("overview")} />
-          <Checkbox {...register("adult")} />
-          <Input {...register("poster_path")} />
-          <Input {...register("stars")} />
+
+          
+          <FormControl isInvalid={errors.title? true : false}>
+            <FormLabel htmlFor='title'>Title</FormLabel>
+            <Input
+              id='title'
+              placeholder='title'
+              {...register('title')}
+            />
+            <FormErrorMessage>
+              {errors.title && errors.title.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors.overview? true : false}>
+            <FormLabel htmlFor='overview'>First name</FormLabel>
+            <Textarea
+              id='overview'
+              placeholder='overview'
+              {...register('overview')}
+            />
+            <FormErrorMessage>
+              {errors.overview && errors.overview.message}
+            </FormErrorMessage>
+          </FormControl>
+          
+
+          <FormControl isInvalid={errors.adult? true : false}>
+            <FormLabel htmlFor='adult'>adult</FormLabel>
+            <Checkbox
+              id='adult'
+              placeholder='adult'
+              {...register("adult")}
+            />
+            <FormErrorMessage>
+              {errors.adult && errors.adult.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors.poster_path? true : false}>
+            <FormLabel htmlFor='poster_path'>Title</FormLabel>
+            <Input
+              id='poster_path'
+              placeholder='poster_path'
+              {...register('poster_path')}
+            />
+            <FormErrorMessage>
+              {errors.poster_path && errors.poster_path.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors.stars? true : false}>
+            <FormLabel htmlFor='stars'>Num stars</FormLabel>
+            <Input
+              id='stars'
+              placeholder='stars'
+              {...register('stars')}
+            />
+            <FormErrorMessage>
+              {errors.stars && errors.stars.message}
+            </FormErrorMessage>
+          </FormControl>
+     
+          <Button  isLoading={isSubmitting} type="submit">Create</Button>
         </form>
       </Container>
     </Layout>
